9cc95a05d51c71c10c503c8ebe872a29a110c08a
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2023-11-09 20:17:31 +0100

    routes in navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
-import { AppProps } from 'next/app';
-import { ThemeProvider } from '@/components/theme-provider';
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Navbar />
       <Component {...pageProps} />
     </ThemeProvider>
   );
